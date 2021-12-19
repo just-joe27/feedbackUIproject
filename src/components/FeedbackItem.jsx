b6bf2e2845d943fd55319a3b70b24287013fd333
Joe Crocker <59538823+just-joe27@users.noreply.github.com>
2021-12-19 06:42:42 +0000

    deploy ready

---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import Card from './shared/Card';
+import { FaTimes, FaEdit } from 'react-icons/fa';
+import FeedbackContext from '../context/FeedbackContext';
+
+function FeedbackItem({ item }) {
+	const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+
+	return (
+		<Card>
+			<div className='num-display'>{item.rating} </div>
+			<button onClick={() => deleteFeedback(item.id)} className='close'>
+				<FaTimes color='purple' />
+			</button>
+			<button className='edit'>
+				<FaEdit onClick={() => editFeedback(item)} color-='purple' />
+			</button>
+			<div className='text-display'>{item.text}</div>
+		</Card>
+	);
+}
+
+export default FeedbackItem;
